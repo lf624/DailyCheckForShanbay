2ddefd9c58c2708fcddc5c7fbc8c38397b563afa
lf624 <55185204+lf624@users.noreply.github.com>
2025-02-06 13:33:47 +0800

    add WxPusher method

--- src/main/java/utils/HttpClientUtil.java
+++ src/main/java/utils/HttpClientUtil.java
@@ -1,9 +1,12 @@
-package util;
+package utils;
 
 import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.hc.core5.http.protocol.HttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,6 +29,23 @@ public class HttpClientUtil {
             httpGet.setHeader("Cookie", cookieHeader);
             HttpContext context = null;
             result = client.execute(httpGet, response -> {
+                if(response.getCode() == 200)
+                    return EntityUtils.toString(response.getEntity());
+                return "";
+            });
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+        return result;
+    }
+
+    public static String doPost(String url, String jsonBody) {
+        String result = "";
+        try(CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(url);
+            StringEntity entity = new StringEntity(jsonBody, ContentType.APPLICATION_JSON);
+            httpPost.setEntity(entity);
+            result = client.execute(httpPost, response -> {
                 if(response.getCode() == 200)
                     return EntityUtils.toString(response.getEntity());
                 return "";
